313f59ccbb334dbe588cef77734f0d1a5987ac61
albtordev <albtordev@gmail.com>
2026-05-27 18:08:28 +0100

    migrated resources

---
+++ datalibraries.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+# pip install scikit-learn
+
+# Create 2D NumPy Array
+def numpy_test():
+    array = np.array([[1,2,3],[4,5,6]])
+    result = array + 10
+    print(result)
+
+
+def pandas_test():
+    data = {'Name': ['John', "George", "Paul","Ringo"], 'Age':[25,30,35]}
+    df = pd.DataFrame(data)
+    filtered_data = df[df['Age']>2825]
+    print(filtered_data)
+
+
+def sklearn_test():
+    iris = load_iris()
+    X, y = iris.data, iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,)
+
+    # train model
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    # evaluate performance
+    score = model.score(X_test, y_test)
+    print(f"Accuracy: {score * 100:.2f}%")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    numpy_test()
+    pandas_test()
+    sklearn_test()
---
+++ webscraping.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup #pip install requests beautifulsoup4
+
+def fetch_links(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = soup.find_all('a',href=True)
+        for link in links:
+            print(link['href'])
+    else:
+        print("Failed to retrieve the webpage")
+
+def retrieve_tags(url, tag):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        headlines = soup.find_all(tag) # class_='article'
+        for headline in headlines:
+            print(headline.text.strip())
+    else:
+        print("Failed to retrieve the webpage")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    fetch_links('https://www.marca.com/')
+    retrieve_tags('https://www.eldia.es/', 'h1')
+
+
