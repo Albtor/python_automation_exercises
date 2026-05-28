1ccd61df3033eccf7fa5cf71e0128f25e19eee57
albtordev <albtordev@gmail.com>
2026-05-28 19:40:19 +0100

    dataset and new methods

--- machinelearning.py
+++ machinelearning.py
@@ -10,6 +10,22 @@ data = pd.read_csv(r'.\resources\datasets\customers.csv')
 print(data.head())
 
 #fill missing values
-data.fillna(method='ffill',inpalce=True)
+data.ffill(inplace=True)
 
-data['Subscription_Type'] = data['Subscription_Type'].map({'Basic':0, 'Premium':1})
+data['Subscription_Type'] = data['Subscription_Type'].map({
+    'Basic': 0,
+    'Standard': 1,
+    'Premium': 2
+})
+# Features and target
+X= data[['Age', 'Subscription_Type', 'Usage']]
+y = data['Churn']
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+
+#Initialize Randon Forest Classifier
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train Model
+model.fit(X_train, y_train)
