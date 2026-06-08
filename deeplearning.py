b8c444ae04d5e18e37a038346a474373475a788c
albtordev <albtordev@gmail.com>
2026-06-08 17:49:17 +0100

    deep learning exercise

---
+++ deeplearning.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import fashion_mnist
+
+# deeplearning / neural network
+# .venv312\Scripts\activate
+# pip install tensorflow
+
+def deeplearning_neuraal_network():
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    x_train = x_train.reshape(-1,28,28,1)
+    x_test = x_test.reshape(-1,28,28,1)
+
+    # BUild CNN Model
+    model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28,28,1)),
+        layers.MaxPooling2D((2,2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2,2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(10, activation='softmax')
+    ])
+
+    # Compile model
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    # Train model
+    model.fit(x_train, y_train, epochs=5)
+    # Evaluate model
+    test_loss, test_acc = model.evaluate(x_test, y_test)
+    print(f"Test accuracy:{test_acc}")
+
+    # Automate model: With cron or task scheduler
