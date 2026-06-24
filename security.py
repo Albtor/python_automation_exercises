111c1d4bea46f7f06849abbd05a08a1f9fed467a
albtordev <albtordev@gmail.com>
2026-06-24 23:46:27 +0100

    security 2

--- security.py
+++ security.py
@@ -1,6 +1,10 @@
 import os
 import pickle
 import google.auth
+from flask import Request
+
+# from google_auth_oauthlib.flow import installed AppFlow
+# from googleapiclient.discovery import build
 
 api_key = os.getenv('API_KEY_DUMMY')
 
@@ -8,4 +12,22 @@ api_key = os.getenv('API_KEY_DUMMY')
 
 
 def oauth20_authentication():
-    print("dummy")
+    SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
+    def authenticate():
+        creds = None
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        return creds
+
+    def list_events():
+        print('Listing events')
+
